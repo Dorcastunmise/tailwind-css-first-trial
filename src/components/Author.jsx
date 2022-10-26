cd80e0ebb9d3cc3046deb3ca8923e9c9b145e1b7
Alimi Oluwatunmise Dorcas <oluwatunmisealimi67@gmail.com>
2022-10-26 08:43:14 +0100

    bug in pictures

--- src/components/Author.jsx
+++ src/components/Author.jsx
@@ -1,8 +1,10 @@
+import face from "../../public/images/mine.jpg";
+
 const Author = () => {
   return (
     <>
         <section className="py-8 px-8 max-w-sm mx-auto bg-slate-100 rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6 my-10">
-            <img className="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" src="./public/mine.jpg" alt="Woman's Face"/>
+            <img className="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" src={face} alt="Woman's Face"/>
                 <div className="text-center space-y-2 sm:text-left">
                     <div className="space-y-0.5">
                         <p className="text-lg text-slate-900 font-semibold">
--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,8 +1,10 @@
+import photo from "../../public/images/logo.jpg";
+
 const Chat = () => {
   return (
         <section className="p-6 max-w-sm mx-auto bg-slate-100 rounded-xl shadow-lg flex items-center space-x-4 my-10">
             <div className="shrink-0">
-                <a href="https://www.microsoft.com/en/microsoft-teams/log-in"><img className="h-12 w-12" src="./public/logo.jpg" alt="ChitChat Logo" />
+                <a href="https://www.microsoft.com/en/microsoft-teams/log-in"><img className="h-12 w-12" src={photo} alt="ChitChat Logo" />
                 </a>
             </div>
             <div>
